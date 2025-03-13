fb8198166425a5000edd6e963765b7ac58e05b50
rb-mv <radzhab._.magomedov@icloud.com>
2025-03-13 07:12:42 +0300

    Add scanner functionality

---
+++ BarcodeScanner/ScannerVC.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerVC.swift
+//  BarcodeScanner
+//
+//  Created by Раджаб Магомедов on 13.03.2025.
+//
+
+import SwiftUI
+
+struct ScannerVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerVC()
+}
