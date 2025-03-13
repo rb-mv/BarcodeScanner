12b2f5443044024372450e014d3450d2f88b41a3
rb-mv <radzhab._.magomedov@icloud.com>
2025-03-13 07:25:46 +0300

    Added error messages

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct BarcodeScannerView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 400)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                Text("Not yet scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
         }
-        .padding()
+        
     }
 }
 
--- BarcodeScanner/ScannerVC.swift
+++ BarcodeScanner/ScannerVC.swift
@@ -5,14 +5,104 @@
 //  Created by Раджаб Магомедов on 13.03.2025.
 //
 
-import SwiftUI
+import UIKit
+import AVFoundation
 
-struct ScannerVC: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+
+enum CameraError: String {
+    case invalidDeviceInput     = "Something is wrong with camera. We are unable to capture the input. :("
+    case invalidScannedValue    = "The value scanned is not valid. This app scans EAN-8 and EAN-13 barcode types."
+    
+    
 }
 
-#Preview {
-    ScannerVC()
+
+
+protocol ScannerVCDelegate: AnyObject {
+    func didFind(barcode: String) //Протокол для общения UIKit (UIKit передает функцию didFind в Coordinator, а он в свою очередь в SwiftUI
+    func didSurface(error: CameraError)
+}
+
+final class ScannerVC: UIViewController {
+    let captureSession = AVCaptureSession() // Захват того, что изображено на камере
+    var previewLayer: AVCaptureVideoPreviewLayer? // та вещь которая отражает вид камеры в опредлённом месте на экране
+    weak var scannerDelegate: ScannerVCDelegate!
+    
+    init(scannerDelegate: ScannerVCDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    
+    
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        } //Чтобы убедиться что у нас есть девайс с которого будет происходит сбор данных
+        
+        
+        let videoInput: AVCaptureDeviceInput
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
+        } else {
+            scannerDelegate.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = .resizeAspectFill // ForceUnwrap потому что в предудущей строке мы его задаем
+        
+        view.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+        
+    }
+    
+}
+
+
+extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let object = metadataObjects.first else {
+            scannerDelegate.didSurface(error: .invalidScannedValue)
+            return
+        }
+        guard let machineReadableObject = object as? AVMetadataMachineReadableCodeObject else {
+            scannerDelegate.didSurface(error: .invalidScannedValue)
+            return
+        }
+        guard let barcode = machineReadableObject.stringValue else {
+            scannerDelegate.didSurface(error: .invalidScannedValue)
+            return
+        }
+        
+        scannerDelegate.didFind(barcode: barcode)
+        
+    }
 }
